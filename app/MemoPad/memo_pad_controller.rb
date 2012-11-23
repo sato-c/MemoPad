2f65d62ce427ad585bac138bde569fc6575be9d7
szk <motosumi64@gmail.com>
2012-11-23 12:13:20 +0900

    日付をデフォルトでいれる/本文がないときはDB登録しない

--- app/MemoPad/memo_pad_controller.rb
+++ app/MemoPad/memo_pad_controller.rb
@@ -1,5 +1,6 @@
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
+require 'date'
 
 class MemoPadController < Rho::RhoController
   include BrowserHelper
@@ -23,6 +24,10 @@ class MemoPadController < Rho::RhoController
   # GET /MemoPad/new
   def new
     @memopad = MemoPad.new
+    
+    # デフォルトで日付をアサインする
+    # フォーム側も日付を入れ込むように編集する
+    @today   = Date.today().to_s()
     render :action => :new, :back => url_for(:action => :index)
   end
 
@@ -38,7 +43,16 @@ class MemoPadController < Rho::RhoController
 
   # POST /MemoPad/create
   def create
-    @memopad = MemoPad.create(@params['memopad'])
+    # 本文がなければ、作らない
+    # タイトルが空欄ならば、デフォルトで"(今日の日付)のメモ"というタイトルにする
+    if @params['memopad']['body'] && @params['memopad']['body'] != ''
+      if @params['memopad']['subject'] == ''
+        @params['memopad']['subject'] = "#{Date.today().to_s()}のメモ"
+      end
+      
+      @memopad = MemoPad.create(@params['memopad'])
+    end
+    
     redirect :action => :index
   end
 
